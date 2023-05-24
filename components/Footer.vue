cbe78b399b39a72bdc861f258084fb89a3a99595
fabianjh11 <fama11u3@gmail.com>
2023-05-23 20:11:33 -0600

    first commit

---
+++ components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <br>
+    <div id="FooterSub" class="w-full bg-[#333333] text-sm text-gray-400">
+        <div class="max-w-[1200px] mx-auto p-4">
+            Programación Para Internet - I5909 - D03 Integrantes: Jiménez Fabian | Manzano Miguel | Mendoza Horacio Universidad de Guadalajara - Ingeniería Informática
+        </div>
+    </div>
+</template>
+
+<style>
+    #FooterSub {
+        text-align: center;
+    }
+</style>
+
+<script setup>
+
+</script>
